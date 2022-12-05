111cdbec8de1b9138264e9daf552a6b7b96bafc6
shangfengli <shangfengli@tencent.com>
2022-12-05 21:34:54 +0800

    leetcode-t094

---
+++ 100/t094-binary-tree-inorder-traversal/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// https://leetcode.cn/problems/binary-tree-inorder-traversal/?favorite=2cktkvj
+
+// 中序遍历，先左，再遍历右边
+
+func main() {
+	root := &TreeNode{
+		Val: 1,
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+			},
+		},
+	}
+
+	fmt.Printf("%v\n", inorderTraversal(root))
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) (res []int) {
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return
+}
