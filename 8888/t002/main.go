790c3e772f6c6ab1facdfda78aa5ac2f4629c0bd
shangfengli <shangfengli@tencent.com>
2022-12-10 09:52:00 +0800

    add 8888-t002

---
+++ 8888/t002/main.go
@@ -0,0 +1,24 @@
+package main
+
+// 题目描述：
+// n 个 三列的 数组，相邻两个数组列不能重复，每组选择一个数据，求最小和
+// [1, 2, 3] [4, 5, 6] [7, 8, 9] sum = 13 （7+5+1）
+
+func main() {
+
+}
+
+func minLatency(latencyes [][]int) int {
+	n := len(latencyes)
+	sum := 0
+	pathNum := make([]int, n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < 3; j++ {
+
+		}
+
+	}
+
+}
+
+
